fcff488d30700d3c209bc79d1a2282292f5f2def
spriteboysz <sprite_bx@126.com>
2022-06-24 00:10:29 +0800

    2022062400

---
+++ src/leetcode/editor/cn/algorithm/P0520.java
@@ -0,0 +1,31 @@
+package leetcode.editor.cn.algorithm;
+
+/**
+ * Author: @Deean
+ * Date: 2022-06-23 23:58
+ * LastEditTime: 2022-06-23 23:58
+ * Description: 检测大写字母
+ */
+
+public class P0520 {
+    // code beginning
+    class Solution {
+        public boolean detectCapitalUse(String word) {
+            int n = word.length(), count = 0;
+            for (int i = 0; i < n; i++) {
+                if (Character.isUpperCase(word.charAt(i))) count++;
+            }
+            return count == n || count == 0 || (count == 1 && Character.isUpperCase(word.charAt(0)));
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new P0520().new Solution();
+        Object ans = s.detectCapitalUse("uSA");
+        System.out.println(ans);
+        ans = s.detectCapitalUse("leetcode");
+        System.out.println(ans);
+        ans = s.detectCapitalUse("LeetcoDe");
+        System.out.println(ans);
+    }
+}
