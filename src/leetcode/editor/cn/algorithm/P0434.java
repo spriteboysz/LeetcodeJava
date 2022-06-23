01afbec55567e49116d1ca1019b95348379a1197
spriteboysz <sprite_bx@126.com>
2022-06-23 23:37:57 +0800

    2022062323

---
+++ src/leetcode/editor/cn/algorithm/P0434.java
@@ -0,0 +1,33 @@
+package leetcode.editor.cn.algorithm;
+
+/**
+ * Author: @Deean
+ * Date: 2022-06-23 23:12
+ * LastEditTime: 2022-06-23 23:12
+ * Description: 字符串中的单词数
+ */
+
+public class P0434 {
+    // code beginning
+    class Solution {
+        public int countSegments(String s) {
+            int count = 0;
+            for (int i = 0; i < s.length(); i++) {
+                if ((i == 0 || s.charAt(i - 1) == ' ') && s.charAt(i) != ' ') {
+                    count += 1;
+                }
+            }
+            return count;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new P0434().new Solution();
+        Object ans = s.countSegments("Hello, my name is John");
+        System.out.println(ans);
+        ans = s.countSegments("");
+        System.out.println(ans);
+        ans = s.countSegments("a   b");
+        System.out.println(ans);
+    }
+}
---
+++ src/leetcode/editor/cn/algorithm/P0461.java
@@ -0,0 +1,29 @@
+package leetcode.editor.cn.algorithm;
+
+/**
+ * Author: @Deean
+ * Date: 2022-06-23 23:32
+ * LastEditTime: 2022-06-23 23:32
+ * Description: 汉明距离
+ */
+
+public class P0461 {
+    // code beginning
+    class Solution {
+        public int hammingDistance(int x, int y) {
+            int hamming = 0;
+            for (int i = 0; i < 32; i++) {
+                hamming += ((x >> i) & 1) ^ ((y >> i) & 1);
+            }
+            return hamming;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new P0461().new Solution();
+        Object ans = s.hammingDistance(1, 4);
+        System.out.println(ans);
+        ans = s.hammingDistance(3, 1);
+        System.out.println(ans);
+    }
+}
