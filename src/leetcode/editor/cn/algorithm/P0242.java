778d6a7d2cc224e401518c5d8de5d732dcf331ce
spriteboysz <sprite_bx@126.com>
2022-06-26 23:48:42 +0800

    2022062623

---
+++ src/leetcode/editor/cn/algorithm/P0242.java
@@ -0,0 +1,32 @@
+package leetcode.editor.cn.algorithm;
+
+/**
+ * Author: Deean
+ * Date: 2022-06-26 22:48
+ * LastEditTime: 2022-06-26 22:48
+ * Description: .
+ */
+
+public class P0242 {
+    // code beginning
+    class Solution {
+        public boolean isAnagram(String s, String t) {
+            if (s.length() != t.length()) return false;
+            int[] chars = new int[26];
+            for (int i = 0; i < s.length(); i++) {
+                chars[s.charAt(i) - 'a']++;
+                chars[t.charAt(i) - 'a']--;
+            }
+            for (int count : chars) {
+                if (count != 0) return false;
+            }
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new P0242().new Solution();
+        Object ans = s.isAnagram("anagram", "nagaram");
+        System.out.println(ans);
+    }
+}
