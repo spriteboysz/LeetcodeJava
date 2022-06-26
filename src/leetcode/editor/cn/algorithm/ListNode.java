008544fb3ecd917cc482b8e4b9d05e88c369c097
spriteboysz <sprite_bx@126.com>
2022-06-26 16:14:20 +0800

    2022062616

--- src/leetcode/editor/cn/algorithm/ListNode.java
+++ src/leetcode/editor/cn/algorithm/ListNode.java
@@ -1,22 +1,21 @@
 package leetcode.editor.cn.algorithm;
 
-
 import java.util.ArrayList;
 
 /**
- * Author: @Deean
- * Date: 2022-06-22 23:09
- * LastEditTime: 2022-06-22 23:09
- * Description: 基础数据结构
+ * Author: Deean
+ * Date: 2022-06-26 16:07
+ * LastEditTime: 2022-06-26 16:07
+ * Description: .
  */
 
-
 //Definition for singly-linked list.
-class ListNode {
+public class ListNode {
+    // code beginning
     int val;
     ListNode next;
 
-    ListNode() {
+    public ListNode() {
     }
 
     ListNode(int val) {
@@ -30,6 +29,7 @@ class ListNode {
 
     ListNode(int[] values) {
         int n = values.length;
+        if (n == 0) new ListNode();
         if (n > 0) {
             ListNode[] nodes = new ListNode[n];
             nodes[0] = new ListNode(values[0]);
@@ -38,6 +38,7 @@ class ListNode {
                 nodes[i - 1].next = nodes[i];
             }
             nodes[nodes.length - 1].next = null;
+            // ListNode(nodes[0].val, nodes[0].next);
             this.val = nodes[0].val;
             this.next = nodes[0].next;
         }
@@ -53,47 +54,9 @@ class ListNode {
         }
         return list.toString();
     }
-}
-
-
-//Definition for a binary tree node.*
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-
-
-public class DataStructure {
 
     public static void main(String[] args) {
-        //ListNode[] nodes = new ListNode[5];
-        //nodes[0] = new ListNode(0);
-        //for (int i = 1; i < nodes.length; i++) {
-        //    nodes[i] = new ListNode(i);
-        //    nodes[i - 1].next = nodes[i];
-        //}
-        //nodes[nodes.length - 1].next = null;
-        //
-        //for (ListNode node : nodes) {
-        //    System.out.println(node.val);
-        //}
-
-        ListNode head = new ListNode(new int[]{});
+        ListNode head = new ListNode(new int[]{1, 2, 3});
         System.out.println(head);
-
     }
 }
---
+++ src/leetcode/editor/cn/algorithm/TreeNode.java
@@ -0,0 +1,33 @@
+package leetcode.editor.cn.algorithm;
+
+
+/**
+ * Author: @Deean
+ * Date: 2022-06-22 23:09
+ * LastEditTime: 2022-06-22 23:09
+ * Description: 基础数据结构
+ */
+
+//Definition for a binary tree node.*
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
