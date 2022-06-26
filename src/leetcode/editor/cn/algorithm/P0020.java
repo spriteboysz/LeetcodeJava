39775e754e7e9254186d9c214fb661c25ee640f0
spriteboysz <sprite_bx@126.com>
2022-06-26 17:11:12 +0800

    2022062617

---
+++ src/leetcode/editor/cn/algorithm/P0020.java
@@ -0,0 +1,39 @@
+package leetcode.editor.cn.algorithm;
+
+import java.util.Stack;
+
+/**
+ * Author: Deean
+ * Date: 2022-06-26 16:39
+ * LastEditTime: 2022-06-26 16:39
+ * Description: 20. 有效的括号
+ */
+
+public class P0020 {
+    // code beginning
+    class Solution {
+        public boolean isValid(String s) {
+            Stack<String> stack = new Stack<>();
+            for (int i = 0; i < s.length(); i++) {
+                String c = String.valueOf(s.charAt(i));
+                int n = stack.size() - 1;
+                if (!stack.isEmpty() && stack.get(n).equals("(") && c.equals(")")) {
+                    stack.pop();
+                } else if (!stack.isEmpty() && stack.get(n).equals("[") && c.equals("]")) {
+                    stack.pop();
+                } else if (!stack.isEmpty() && stack.get(n).equals("{") && c.equals("}")) {
+                    stack.pop();
+                } else {
+                    stack.add(c);
+                }
+            }
+            return stack.size() == 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new P0020().new Solution();
+        Object ans = s.isValid("([)]");
+        System.out.println(ans);
+    }
+}
