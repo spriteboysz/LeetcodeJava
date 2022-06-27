665e8300cc4702cd3da725e33c6fc314bdc3d1ea
spriteboysz <sprite_bx@126.com>
2022-06-28 00:11:59 +0800

    2022062800

---
+++ src/leetcode/editor/cn/algorithm/P0225.java
@@ -0,0 +1,52 @@
+package leetcode.editor.cn.algorithm;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Author: Deean
+ * Date: 2022-06-28 00:01
+ * LastEditTime: 2022-06-28 00:01
+ * Description: .
+ */
+
+public class P0225 {
+    // code beginning
+    class MyStack {
+        Queue<Integer> queue1;
+        Queue<Integer> queue2;
+
+        public MyStack() {
+            queue1 = new LinkedList<>();
+            queue2 = new LinkedList<>();
+        }
+
+        public void push(int x) {
+            queue2.offer(x);
+            while (!queue1.isEmpty()) {
+                queue2.offer(queue1.poll());
+            }
+            Queue<Integer> temp = queue1;
+            queue1 = queue2;
+            queue2 = temp;
+        }
+
+        public int pop() {
+            return queue1.poll();
+        }
+
+        public int top() {
+            return queue1.peek();
+        }
+
+        public boolean empty() {
+            return queue1.isEmpty();
+        }
+    }
+
+    public static void main(String[] args) {
+        MyStack s = new P0225().new MyStack();
+        Object ans = s.pop();
+        System.out.println(ans);
+    }
+}
