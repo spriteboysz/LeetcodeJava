f725a7bae0c43aed60ce1f1992c19f3ed8145907
spriteboysz <sprite_bx@126.com>
2022-06-29 00:07:03 +0800

    2022062900

---
+++ src/leetcode/editor/cn/algorithm/P0728.java
@@ -0,0 +1,42 @@
+package leetcode.editor.cn.algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Author: Deean
+ * Date: 2022-06-28 23:58
+ * LastEditTime: 2022-06-28 23:58
+ * Description: 728. 自除数
+ */
+
+public class P0728 {
+    // code beginning
+    class Solution {
+        private boolean process(int num) {
+            int tmp = num;
+            while (tmp > 0) {
+                int cur = tmp % 10;
+                if (cur == 0 || num % cur != 0) return false;
+                tmp /= 10;
+            }
+            return true;
+        }
+
+        public List<Integer> selfDividingNumbers(int left, int right) {
+            List<Integer> nums = new ArrayList<>();
+            for (int i = left; i <= right; i++) {
+                if (process(i)) {
+                    nums.add(i);
+                }
+            }
+            return nums;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new P0728().new Solution();
+        Object ans = s.selfDividingNumbers(47, 85);
+        System.out.println(ans);
+    }
+}
