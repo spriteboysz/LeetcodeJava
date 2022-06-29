b1ae00f50d151291a4d86e4e6f14db769f8aa604
spriteboysz <sprite_bx@126.com>
2022-06-30 00:15:50 +0800

    2022063000

--- src/leetcode/editor/cn/algorithm/TreeNode.java
+++ src/leetcode/editor/cn/algorithm/TreeNode.java
@@ -1,6 +1,9 @@
 package leetcode.editor.cn.algorithm;
 
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Author: @Deean
  * Date: 2022-06-22 23:09
@@ -27,18 +30,56 @@ public class TreeNode {
         this.right = right;
     }
 
+    TreeNode(String data) {
+        if (data.equals("")) {
+            this.val = 0;
+        }
+        String[] dataList = data.substring(1, data.length() - 1).split(",");
+        TreeNode root = new TreeNode(Integer.parseInt(dataList[0]));
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int i = 1;
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (!"null".equals(dataList[i])) {
+                node.left = new TreeNode(Integer.parseInt(dataList[i]));
+                queue.offer(node.left);
+            }
+            i++;
+            if (!"null".equals(dataList[i])) {
+                node.right = new TreeNode(Integer.parseInt(dataList[i]));
+                queue.offer(node.right);
+            }
+            i++;
+        }
+        this.val = root.val;
+        this.left = root.left;
+        this.right = root.right;
+    }
 
     @Override
     public String toString() {
-        return "TreeNode{" +
-                "val=" + val +
-                ", left=" + left +
-                ", right=" + right +
-                '}';
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(this);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node != null) {
+                sb.append("").append(node.val);
+                queue.offer(node.left);
+                queue.offer(node.right);
+            } else {
+                sb.append("null");
+            }
+            sb.append(",");
+        }
+        sb.append("]");
+        return sb.toString();
     }
 
     public static void main(String[] args) {
-        //TreeNode tree = new TreeNode(ArrayList(new int[]{1, 2, 3, null, 4}));
-        //System.out.println(tree);
+        TreeNode root = new TreeNode("1,2,3");
+        System.out.println(root);
     }
 }
