e03c3a0744bcacb383e527271e77c7081f0449c7
spriteboysz <sprite_bx@126.com>
2022-06-29 23:09:06 +0800

    2022062923

---
+++ src/leetcode/editor/cn/algorithm/P2011.java
@@ -0,0 +1,27 @@
+package leetcode.editor.cn.algorithm;
+
+/**
+ * Author: Deean
+ * Date: 2022-06-29 22:59
+ * LastEditTime: 2022-06-29 22:59
+ * Description: 2011. 执行操作后的变量值
+ */
+
+public class P2011 {
+    // code beginning
+    class Solution {
+        public int finalValueAfterOperations(String[] operations) {
+            int value = 0;
+            for (String operation : operations) {
+                value += (operation.charAt(1) == '+') ? 1 : -1;
+            }
+            return value;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new P2011().new Solution();
+        Object ans = s.finalValueAfterOperations(new String[]{"++X", "++X", "X++"});
+        System.out.println(ans);
+    }
+}
