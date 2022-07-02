81ae83547fc582be18473e3ea787e41e93a4140c
spriteboysz <sprite_bx@126.com>
2022-07-02 22:14:43 +0800

    2022070222

---
+++ src/leetcode/editor/cn/algorithm/P1365.java
@@ -0,0 +1,31 @@
+package leetcode.editor.cn.algorithm;
+
+import java.util.Arrays;
+
+/**
+ * Author: Deean
+ * Date: 2022-07-02 21:48
+ * LastEditTime: 2022-07-02 21:48
+ * Description: 1365. 有多少小于当前数字的数字
+ */
+
+public class P1365 {
+    // code beginning
+    class Solution {
+        public int[] smallerNumbersThanCurrent(int[] nums) {
+            int[] count = new int[nums.length];
+            for (int i = 0; i < nums.length; i++) {
+                for (int num : nums) {
+                    if (nums[i] > num) count[i]++;
+                }
+            }
+            return count;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new P1365().new Solution();
+        int[] ans = s.smallerNumbersThanCurrent(new int[]{6, 5, 4, 8});
+        System.out.println(Arrays.toString(ans));
+    }
+}
--- src/leetcode/editor/cn/algorithm/P1588.java
+++ src/leetcode/editor/cn/algorithm/P1588.java
@@ -10,12 +10,22 @@ package leetcode.editor.cn.algorithm;
 public class P1588 {
     // code beginning
     class Solution {
-
+        public int sumOddLengthSubarrays(int[] arr) {
+            int sum = 0;
+            for (int start = 0; start < arr.length; start += 1) {
+                for (int len = 1; start + len <= arr.length; len += 2) {
+                    for (int i = start; i < start + len; i++) {
+                        sum += arr[i];
+                    }
+                }
+            }
+            return sum;
+        }
     }
 
     public static void main(String[] args) {
         Solution s = new P1588().new Solution();
-        Object ans = s.;
+        Object ans = s.sumOddLengthSubarrays(new int[]{1, 4, 2, 5, 3});
         System.out.println(ans);
     }
 }
