aabc916d5fecde2a4cd4439ec6c691cf7cbf2ff5
spriteboysz <sprite_bx@126.com>
2022-07-03 23:37:35 +0800

    2022070323

---
+++ src/leetcode/editor/cn/algorithm/P1859.java
@@ -0,0 +1,39 @@
+package leetcode.editor.cn.algorithm;
+
+import java.util.HashMap;
+
+/**
+ * Author: Deean
+ * Date: 2022-07-03 23:26
+ * LastEditTime: 2022-07-03 23:26
+ * Description: 1859. 将句子排序
+ */
+
+public class P1859 {
+    // code beginning
+    class Solution {
+        public String sortSentence(String s) {
+            HashMap<Integer, String> hashMap = new HashMap<>();
+            String[] words = s.split(" ");
+            for (String word : words) {
+                String value = word.substring(0, word.length() - 1);
+                int key = Integer.parseInt(word.substring(word.length() - 1));
+                hashMap.put(key, value);
+            }
+
+            StringBuilder sentence = new StringBuilder();
+            for (int i = 0; i < 10; i++) {
+                if (hashMap.containsKey(i)) {
+                    sentence.append(hashMap.get(i)).append(" ");
+                }
+            }
+            return sentence.toString().trim();
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new P1859().new Solution();
+        Object ans = s.sortSentence("Myself2 Me1 I4 and3");
+        System.out.println(ans);
+    }
+}
---
+++ src/leetcode/editor/cn/algorithm/P2169.java
@@ -0,0 +1,32 @@
+package leetcode.editor.cn.algorithm;
+
+/**
+ * Author: Deean
+ * Date: 2022-07-03 23:08
+ * LastEditTime: 2022-07-03 23:08
+ * Description: 2169. 得到 0 的操作数
+ */
+
+public class P2169 {
+    // code beginning
+    class Solution {
+        public int countOperations(int num1, int num2) {
+            int count = 0;
+            while (num1 != 0 && num2 != 0) {
+                if (num1 > num2) {
+                    num1 -= num2;
+                } else {
+                    num2 -= num1;
+                }
+                count++;
+            }
+            return count;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new P2169().new Solution();
+        Object ans = s.countOperations(2, 3);
+        System.out.println(ans);
+    }
+}
