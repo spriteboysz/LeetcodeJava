13b3a0f1b5f399841c4d1e4cb84c43e098e8ecf7
spriteboysz <sprite_bx@126.com>
2022-07-04 23:27:41 +0800

    2022070423

---
+++ src/leetcode/editor/cn/algorithm/P1656.java
@@ -0,0 +1,42 @@
+package leetcode.editor.cn.algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Author: Deean
+ * Date: 2022-07-04 23:18
+ * LastEditTime: 2022-07-04 23:18
+ * Description: 1656. 设计有序流
+ */
+
+public class P1656 {
+    // code beginning
+    class OrderedStream {
+        private int index = 1;
+        private final String[] stream;
+
+        public OrderedStream(int n) {
+            stream = new String[n + 1];
+        }
+
+        public List<String> insert(int idKey, String value) {
+            stream[idKey] = value;
+            List<String> list = new ArrayList<>();
+            while (index < stream.length && stream[index] != null) {
+                list.add(stream[index++]);
+            }
+            return list;
+        }
+    }
+
+    public static void main(String[] args) {
+        OrderedStream os = new P1656().new OrderedStream(5);
+        System.out.println(os.insert(3, "ccccc")); // 插入 (3, "ccccc")，返回 []
+        System.out.println(os.insert(1, "aaaaa")); // 插入 (1, "aaaaa")，返回 ["aaaaa"]
+        System.out.println(os.insert(2, "bbbbb")); // 插入 (2, "bbbbb")，返回 ["bbbbb", "ccccc"]
+        System.out.println(os.insert(5, "eeeee")); // 插入 (5, "eeeee")，返回 []
+        System.out.println(os.insert(4, "ddddd")); // 插入 (4, "ddddd")，返回 ["ddddd", "eeeee"]
+
+    }
+}
