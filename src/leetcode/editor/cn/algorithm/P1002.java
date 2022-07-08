d6dc607cb7e963847f58a4f11b4627c1092c649c
spriteboysz <sprite_bx@126.com>
2022-07-08 22:56:32 +0800

    2022070822

---
+++ src/leetcode/editor/cn/algorithm/P1002.java
@@ -0,0 +1,26 @@
+package leetcode.editor.cn.algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Author: Deean
+ * Date: 2022-07-08 22:51
+ * LastEditTime: 2022-07-08 22:51
+ * Description: 1002. 查找共用字符
+ */
+
+public class P1002 {
+    // code beginning
+    class Solution {
+        public List<String> commonChars(String[] words) {
+            return new ArrayList<>();
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new P1002().new Solution();
+        Object ans = s.commonChars(new String[]{"cool", "lock", "cook"});
+        System.out.println(ans);
+    }
+}
