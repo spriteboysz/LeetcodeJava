691b2e00406230a7bb808d2307b81bddb56b09b1
spriteboysz <sprite_bx@126.com>
2022-07-09 00:11:52 +0800

    2022070900

---
+++ src/leetcode/editor/cn/algorithm/P0412.java
@@ -0,0 +1,39 @@
+package leetcode.editor.cn.algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Author: Deean
+ * Date: 2022-07-08 23:41
+ * LastEditTime: 2022-07-08 23:41
+ * Description: 412. Fizz Buzz
+ */
+
+public class P0412 {
+    // code beginning
+    class Solution {
+        public List<String> fizzBuzz(int n) {
+            List<String> list = new ArrayList<>();
+            for (int i = 1; i <= n; i++) {
+                if (i % 3 == 0 && i % 5 == 0) {
+                    list.add("FizzBuzz");
+                } else if (i % 3 == 0) {
+                    list.add("Fizz");
+                } else if (i % 5 == 0) {
+                    list.add("Buzz");
+                } else {
+                    list.add(String.valueOf(i));
+                }
+            }
+            return list;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Solution s = new P0412().new Solution();
+        Object ans = s.fizzBuzz(15);
+        System.out.println(ans);
+    }
+}
