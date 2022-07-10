5b0e882a8b707102f4005e54deb8c8d621307282
spriteboysz <sprite_bx@126.com>
2022-07-10 22:58:05 +0800

    2022071022

---
+++ src/leetcode/editor/cn/algorithm/P1089.java
@@ -0,0 +1,22 @@
+package leetcode.editor.cn.algorithm;
+
+/**
+ * Author: Deean
+ * Date: 2022-07-10 22:54
+ * LastEditTime: 2022-07-10 22:54
+ * Description: 1089. 复写零
+ */
+
+public class P1089 {
+    // code beginning
+    class Solution {
+        public void duplicateZeros(int[] arr) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new P1089().new Solution();
+        s.duplicateZeros(new int[]{1, 0, 2, 3, 0, 4, 5, 0});
+    }
+}
