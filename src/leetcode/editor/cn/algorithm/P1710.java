950d78b1d843b1c06035ee53151b65b318df0491
spriteboysz <sprite_bx@126.com>
2022-07-10 17:26:07 +0800

    2022071017

---
+++ src/leetcode/editor/cn/algorithm/P1710.java
@@ -0,0 +1,36 @@
+package leetcode.editor.cn.algorithm;
+
+import java.util.Arrays;
+
+/**
+ * Author: Deean
+ * Date: 2022-07-10 17:18
+ * LastEditTime: 2022-07-10 17:18
+ * Description: 1710. 卡车上的最大单元数
+ */
+
+public class P1710 {
+    // code beginning
+    class Solution {
+        public int maximumUnits(int[][] boxTypes, int truckSize) {
+            Arrays.sort(boxTypes, (o1, o2) -> o2[1] - o1[1]);
+            int count = 0;
+            for (int[] box : boxTypes) {
+                if (box[0] < truckSize) {
+                    count += box[0] * box[1];
+                    truckSize -= box[0];
+                } else {
+                    count += truckSize * box[1];
+                    return count;
+                }
+            }
+            return count;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new P1710().new Solution();
+        Object ans = s.maximumUnits(new int[][]{{5, 10}, {2, 5}, {4, 7}, {3, 9}}, 10);
+        System.out.println(ans);
+    }
+}
