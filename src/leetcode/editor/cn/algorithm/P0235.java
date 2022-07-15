8356b65632a9e108f78dcb0b2e78eecd92e1eace
spriteboysz <sprite_bx@126.com>
2022-07-15 23:04:47 +0800

    2022071523

---
+++ src/leetcode/editor/cn/algorithm/P0235.java
@@ -0,0 +1,32 @@
+package leetcode.editor.cn.algorithm;
+
+import leetcode.editor.cn.common.TreeNode;
+
+/**
+ * Author: Deean
+ * Date: 2022-07-15 22:50
+ * LastEditTime: 2022-07-15 22:50
+ * Description: 235. 二叉搜索树的最近公共祖先
+ */
+
+public class P0235 {
+    // code beginning
+    class Solution {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            if (p.val < root.val && q.val < root.val) {
+                return lowestCommonAncestor(root.left, p, q);
+            } else if (p.val > root.val && q.val > root.val) {
+                return lowestCommonAncestor(root.right, p, q);
+            } else {
+                return root;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new P0235().new Solution();
+        Object ans = s.lowestCommonAncestor(new TreeNode("[6,2,8,0,4,7,9,null,null,3,5]"), new TreeNode(2),
+                new TreeNode(4));
+        System.out.println(ans);
+    }
+}
---
+++ src/leetcode/editor/cn/interview/M00080003.java
@@ -0,0 +1,33 @@
+package leetcode.editor.cn.interview;
+
+/**
+ * Author: Deean
+ * Date: 2022-07-15 22:54
+ * LastEditTime: 2022-07-15 22:54
+ * Description: 面试题 08.03. 魔术索引
+ */
+
+public class M00080003 {
+    // code beginning
+    class Solution {
+        public int findMagicIndex(int[] nums) {
+            int index = 0;
+            while (index < nums.length) {
+                if (nums[index] == index) {
+                    return index;
+                } else if (nums[index] > index) {
+                    index = nums[index];
+                } else {
+                    index++;
+                }
+            }
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new M00080003().new Solution();
+        Object ans = s.findMagicIndex(new int[]{0, 2, 3, 4, 5});
+        System.out.println(ans);
+    }
+}
