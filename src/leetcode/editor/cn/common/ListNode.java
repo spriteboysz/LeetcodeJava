47e6920378140549bb8d7e9e5f82e1c7cc43d5e8
spriteboysz <sprite_bx@126.com>
2022-07-16 10:49:40 +0800

    2022071610

--- src/leetcode/editor/cn/common/ListNode.java
+++ src/leetcode/editor/cn/common/ListNode.java
@@ -18,11 +18,11 @@ public class ListNode {
     public ListNode() {
     }
 
-    ListNode(int val) {
+    public ListNode(int val) {
         this.val = val;
     }
 
-    ListNode(int val, ListNode next) {
+    public ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
     }
