27e6818aea2c290977ff8d38e30e5fca2c378d7b
spriteboysz <sprite_bx@126.com>
2022-07-17 16:13:39 +0800

    2022071716

--- src/leetcode/editor/cn/sword/O0056II.java
+++ src/leetcode/editor/cn/sword/O0056II.java
@@ -1,33 +1,36 @@
 package leetcode.editor.cn.sword;
 
-import leetcode.editor.cn.common.TreeNode;
-
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Author: Deean
- * Date: 2022-07-08 22:28
- * LastEditTime: 2022-07-08 22:28
- * Description: 剑指 Offer II 056. 二叉搜索树中两个节点之和
+ * Date: 2022-07-17 16:02
+ * LastEditTime: 2022-07-17 16:02
+ * Description: 剑指 Offer 56 - II. 数组中数字出现的次数 II
  */
 
 public class O0056II {
     // code beginning
     class Solution {
-        Set<Integer> set = new HashSet<>();
-
-        public boolean findTarget(TreeNode root, int k) {
-            if (root == null) return false;
-            if (set.contains(k - root.val)) return true;
-            set.add(root.val);
-            return findTarget(root.left, k) || findTarget(root.right, k);
+        public int singleNumber(int[] nums) {
+            HashMap<Integer, Integer> count = new HashMap<>();
+            for (int num : nums) {
+                count.put(num, count.getOrDefault(num, 0) + 1);
+            }
+            //for (int num : nums) {
+            //    if (count.get(num) == 1) return num;
+            //}
+            for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+                if (entry.getValue() == 1) return entry.getKey();
+            }
+            return -1;
         }
     }
 
     public static void main(String[] args) {
         Solution s = new O0056II().new Solution();
-        Object ans = s.findTarget(new TreeNode("[8,6,10,5,7,9,11]"), 22);
+        Object ans = s.singleNumber(new int[]{9, 1, 7, 9, 7, 9, 7});
         System.out.println(ans);
     }
 }
