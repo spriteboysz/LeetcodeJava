f5ec30536f2879077195a6e584d441b3562d76f4
spriteboysz <sprite_bx@126.com>
2022-07-19 00:09:29 +0800

    2022071900

---
+++ src/leetcode/editor/cn/algorithm/P1161.java
@@ -0,0 +1,47 @@
+package leetcode.editor.cn.algorithm;
+
+import leetcode.editor.cn.common.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Author: Deean
+ * Date: 2022-07-18 23:31
+ * LastEditTime: 2022-07-18 23:31
+ * Description: 1161. 最大层内元素和
+ */
+
+public class P1161 {
+    // code beginning
+    class Solution {
+        public int maxLevelSum(TreeNode root) {
+            if (root == null) return 0;
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+            int max = Integer.MIN_VALUE, maxLevel = 0, level = 1;
+            while (!queue.isEmpty()) {
+                int sum = 0, size = queue.size();
+                for (int i = 0; i < size; i++) {
+                    TreeNode node = queue.poll();
+                    assert node != null;
+                    sum += node.val;
+                    if (node.left != null) queue.offer(node.left);
+                    if (node.right != null) queue.offer(node.right);
+                }
+                if (sum > max) {
+                    max = sum;
+                    maxLevel = level;
+                }
+                level++;
+            }
+            return maxLevel;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new P1161().new Solution();
+        Object ans = s.maxLevelSum(new TreeNode("[989,null,10250,98693,-89388,null,null,null,-32127]"));
+        System.out.println(ans);
+    }
+}
---
+++ src/leetcode/editor/cn/algorithm/P1302.java
@@ -0,0 +1,43 @@
+package leetcode.editor.cn.algorithm;
+
+import leetcode.editor.cn.common.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Author: Deean
+ * Date: 2022-07-18 23:41
+ * LastEditTime: 2022-07-18 23:41
+ * Description: 1302. 层数最深叶子节点的和
+ */
+
+public class P1302 {
+    // code beginning
+    class Solution {
+        public int deepestLeavesSum(TreeNode root) {
+            int lastSum = 0;
+            if (root == null) return 0;
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+            while (!queue.isEmpty()) {
+                int sum = 0, size = queue.size();
+                for (int i = 0; i < size; i++) {
+                    TreeNode node = queue.poll();
+                    assert node != null;
+                    sum += node.val;
+                    if (node.left != null) queue.offer(node.left);
+                    if (node.right != null) queue.offer(node.right);
+                }
+                lastSum = sum;
+            }
+            return lastSum;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new P1302().new Solution();
+        Object ans = s.deepestLeavesSum(new TreeNode("[6,7,8,2,7,1,3,9,null,1,4,null,null,null,5]"));
+        System.out.println(ans);
+    }
+}
