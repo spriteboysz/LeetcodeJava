ebc3061bb8f600e7f3866647173172284eeafe14
spriteboysz <sprite_bx@126.com>
2022-07-23 22:30:01 +0800

    2022072322

---
+++ src/leetcode/editor/cn/interview/M00100011.java
@@ -0,0 +1,31 @@
+package leetcode.editor.cn.interview;
+
+import java.util.Arrays;
+
+/**
+ * Author: Deean
+ * Date: 2022-07-23 22:04
+ * LastEditTime: 2022-07-23 22:04
+ * Description: 面试题 10.11. 峰与谷
+ */
+
+public class M00100011 {
+    // code beginning
+    class Solution {
+        public void wiggleSort(int[] nums) {
+            Arrays.sort(nums);
+            for (int i = 0; i < nums.length - 1; i += 2) {
+                int temp = nums[i];
+                nums[i] = nums[i + 1];
+                nums[i + 1] = temp;
+            }
+            // System.out.println(Arrays.toString(nums));
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new M00100011().new Solution();
+        s.wiggleSort(new int[]{5, 3, 1, 2, 3});
+        System.out.println("SUCCESS");
+    }
+}
