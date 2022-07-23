85ebfb6b3849652c3f478e96432fea630087c57f
spriteboysz <sprite_bx@126.com>
2022-07-23 14:22:46 +0800

    2022072314

---
+++ src/leetcode/editor/cn/algorithm/P0046.java
@@ -0,0 +1,49 @@
+package leetcode.editor.cn.algorithm;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Author: Deean
+ * Date: 2022-07-23 14:13
+ * LastEditTime: 2022-07-23 14:13
+ * Description: 46. 全排列
+ */
+
+public class P0046 {
+    // code beginning
+    class Solution {
+        List<List<Integer>> paths = new ArrayList<>();
+        LinkedList<Integer> path = new LinkedList<>();
+        boolean[] visited;
+
+        public List<List<Integer>> permute(int[] nums) {
+            if (nums.length == 0) return paths;
+            visited = new boolean[nums.length];
+            backtrace(nums);
+            return paths;
+        }
+
+        private void backtrace(int[] nums) {
+            if (path.size() == nums.length) {
+                paths.add(new ArrayList<>(path));
+                return;
+            }
+            for (int i = 0; i < nums.length; i++) {
+                if (visited[i]) continue;
+                visited[i] = true;
+                path.add(nums[i]);
+                backtrace(nums);
+                path.removeLast();
+                visited[i] = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new P0046().new Solution();
+        Object ans = s.permute(new int[]{1, 2, 3});
+        System.out.println(ans);
+    }
+}
---
+++ src/leetcode/editor/cn/algorithm/P0216.java
@@ -0,0 +1,46 @@
+package leetcode.editor.cn.algorithm;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Author: Deean
+ * Date: 2022-07-23 13:52
+ * LastEditTime: 2022-07-23 13:52
+ * Description: 216. 组合总和 III
+ */
+
+public class P0216 {
+    // code beginning
+    class Solution {
+        List<List<Integer>> paths = new ArrayList<>();
+        LinkedList<Integer> path = new LinkedList<>();
+
+        public List<List<Integer>> combinationSum3(int k, int n) {
+            backtrace(n, k, 1, 0);
+            return paths;
+        }
+
+        private void backtrace(int target, int k, int start, int sum) {
+            if (sum > target) return;
+            if (path.size() == k) {
+                if (sum == target) paths.add(new ArrayList<>(path));
+                return;
+            }
+            for (int i = start; i <= 9 - (k - path.size()) + 1; i++) {
+                path.add(i);
+                sum += i;
+                backtrace(target, k, i + 1, sum);
+                path.removeLast();
+                sum -= i;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new P0216().new Solution();
+        Object ans = s.combinationSum3(3, 9);
+        System.out.println(ans);
+    }
+}
