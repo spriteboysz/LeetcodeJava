1e7bc094fd39f68b0690503ac9d8d125976e3052
spriteboysz <sprite_bx@126.com>
2022-07-26 23:40:07 +0800

    2022072623

---
+++ src/leetcode/editor/cn/algorithm/P0877.java
@@ -0,0 +1,23 @@
+package leetcode.editor.cn.algorithm;
+
+/**
+ * Author: Deean
+ * Date: 2022-07-26 23:28
+ * LastEditTime: 2022-07-26 23:28
+ * Description: 877. 石子游戏
+ */
+
+public class P0877 {
+    // code beginning
+    class Solution {
+        public boolean stoneGame(int[] piles) {
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new P0877().new Solution();
+        Object ans = s.stoneGame(new int[]{3, 7, 2, 3});
+        System.out.println(ans);
+    }
+}
