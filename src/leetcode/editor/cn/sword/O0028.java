29190f22e221512f758361e8f19752895e5144da
spriteboysz <sprite_bx@126.com>
2022-07-30 00:07:51 +0800

    2022073000

---
+++ src/leetcode/editor/cn/sword/O0028.java
@@ -0,0 +1,31 @@
+package leetcode.editor.cn.sword;
+
+import leetcode.editor.cn.common.TreeNode;
+
+/**
+ * Author: Deean
+ * Date: 2022-07-29 23:44
+ * LastEditTime: 2022-07-29 23:44
+ * Description: 剑指 Offer 28. 对称的二叉树
+ */
+
+public class O0028 {
+    // code beginning
+    class Solution {
+        public boolean isSymmetric(TreeNode root) {
+            return dfs(root, root);
+        }
+
+        private boolean dfs(TreeNode root1, TreeNode root2) {
+            if (root1 == null && root2 == null) return true;
+            if (root1 == null || root2 == null) return false;
+            return root1.val == root2.val && dfs(root1.left, root2.right) && dfs(root1.right, root2.left);
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new O0028().new Solution();
+        Object ans = s.isSymmetric(new TreeNode("[1,2,2,3,4,4,3]"));
+        System.out.println(ans);
+    }
+}
---
+++ src/leetcode/editor/cn/sword/OII0018.java
@@ -0,0 +1,29 @@
+package leetcode.editor.cn.sword;
+
+/**
+ * Author: Deean
+ * Date: 2022-07-29 23:39
+ * LastEditTime: 2022-07-29 23:39
+ * Description: 剑指 Offer II 018. 有效的回文
+ */
+
+public class OII0018 {
+    // code beginning
+    class Solution {
+        public boolean isPalindrome(String s) {
+            StringBuilder builder = new StringBuilder();
+            for (char c : s.toCharArray()) {
+                if (Character.isLetterOrDigit(c)) {
+                    builder.append(Character.toLowerCase(c));
+                }
+            }
+            return builder.toString().equals(builder.reverse().toString());
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new OII0018().new Solution();
+        Object ans = s.isPalindrome("A man, a plan, a canal: Panama");
+        System.out.println(ans);
+    }
+}
