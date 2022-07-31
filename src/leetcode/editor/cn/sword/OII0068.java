7921094a161605914a9daf85976062cf9c8a38fb
spriteboysz <sprite_bx@126.com>
2022-07-31 23:36:17 +0800

    2022073123

---
+++ src/leetcode/editor/cn/sword/OII0068.java
@@ -0,0 +1,33 @@
+package leetcode.editor.cn.sword;
+
+/**
+ * Author: Deean
+ * Date: 2022-07-31 23:30
+ * LastEditTime: 2022-07-31 23:30
+ * Description: 剑指 Offer II 068. 查找插入位置
+ */
+
+public class OII0068 {
+    // code beginning
+    class Solution {
+        public int searchInsert(int[] nums, int target) {
+            int left = 0, right = nums.length - 1, index = nums.length;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] >= target) {
+                    index = mid;
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            return index;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new OII0068().new Solution();
+        Object ans = s.searchInsert(new int[]{1, 3, 5, 6}, 2);
+        System.out.println(ans);
+    }
+}
---
+++ src/leetcode/editor/cn/sword/OII0072.java
@@ -0,0 +1,35 @@
+package leetcode.editor.cn.sword;
+
+/**
+ * Author: Deean
+ * Date: 2022-07-31 23:22
+ * LastEditTime: 2022-07-31 23:22
+ * Description: 剑指 Offer II 072. 求平方根
+ */
+
+public class OII0072 {
+    // code beginning
+    class Solution {
+        public int mySqrt(int x) {
+            int left = 0, right = x, sqrt = -1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if ((long) mid * mid <= x) {
+                    sqrt = mid;
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            return sqrt;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new OII0072().new Solution();
+        Object ans = s.mySqrt(4);
+        System.out.println(ans);
+        ans = s.mySqrt(8);
+        System.out.println(ans);
+    }
+}
