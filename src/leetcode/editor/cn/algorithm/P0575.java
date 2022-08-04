8c885746e189079e1817ad1595bab7761673b646
spriteboysz <sprite_bx@126.com>
2022-08-04 23:35:25 +0800

    2022080423

---
+++ src/leetcode/editor/cn/algorithm/P0575.java
@@ -0,0 +1,29 @@
+package leetcode.editor.cn.algorithm;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Author: Deean
+ * Date: 2022-08-04 23:30
+ * Description: 575. 分糖果
+ */
+
+public class P0575 {
+    // code beginning
+    class Solution {
+        public int distributeCandies(int[] candyType) {
+            Set<Integer> set = new HashSet<>();
+            for (int type : candyType) {
+                set.add(type);
+            }
+            return Math.min(set.size(), candyType.length / 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new P0575().new Solution();
+        Object ans = s.distributeCandies(new int[]{1, 1, 2, 3});
+        System.out.println(ans);
+    }
+}
