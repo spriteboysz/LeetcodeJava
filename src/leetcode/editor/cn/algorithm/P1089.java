26abdcf76e5549ae691a183b33b3cd5c338a5963
spriteboysz <sprite_bx@126.com>
2022-08-06 20:05:22 +0800

    2022080620

--- src/leetcode/editor/cn/algorithm/P1089.java
+++ src/leetcode/editor/cn/algorithm/P1089.java
@@ -1,5 +1,9 @@
 package leetcode.editor.cn.algorithm;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Author: Deean
  * Date: 2022-07-10 22:54
@@ -11,7 +15,15 @@ public class P1089 {
     // code beginning
     class Solution {
         public void duplicateZeros(int[] arr) {
-
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < arr.length && list.size() < arr.length; i++) {
+                list.add(arr[i]);
+                if (arr[i] == 0) list.add(0);
+            }
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = list.get(i);
+            }
+            System.out.println(Arrays.toString(arr));
         }
     }
 
