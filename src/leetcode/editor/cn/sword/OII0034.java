98df43bb310b51df572c35322b4829242744f514
spriteboysz <sprite_bx@126.com>
2022-08-07 23:51:35 +0800

    2022080723

---
+++ src/leetcode/editor/cn/sword/OII0034.java
@@ -0,0 +1,39 @@
+package leetcode.editor.cn.sword;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Author: Deean
+ * Date: 2022-08-07 23:26
+ * Description: 剑指 Offer II 034. 外星语言是否排序
+ */
+
+public class OII0034 {
+    // code beginning
+    class Solution {
+        public boolean isAlienSorted(String[] words, String order) {
+            Map<Character, Character> map = new HashMap<>();
+            for (int i = 0; i < order.length(); i++) {
+                char c = order.charAt(i);
+                map.put(c, (char) (i + 'a'));
+            }
+            String temp = "0";
+            for (String word : words) {
+                StringBuilder builder = new StringBuilder();
+                for (char c : word.toCharArray()) {
+                    builder.append(map.get(c));
+                }
+                if (builder.toString().compareTo(temp) < 0) return false;
+                temp = builder.toString();
+            }
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new OII0034().new Solution();
+        Object ans = s.isAlienSorted(new String[]{"word", "world", "row"}, "worldabcefghijkmnpqstuvxyz");
+        System.out.println(ans);
+    }
+}
---
+++ src/leetcode/editor/cn/sword/OII0073.java
@@ -0,0 +1,43 @@
+package leetcode.editor.cn.sword;
+
+/**
+ * Author: Deean
+ * Date: 2022-08-07 23:40
+ * Description: 剑指 Offer II 073. 狒狒吃香蕉
+ */
+
+public class OII0073 {
+    // code beginning
+    class Solution {
+        public int minEatingSpeed(int[] piles, int h) {
+            int left = 1, right = 0;
+            for (int pile : piles) right = Math.max(right, pile);
+            while (left < right) {
+                int mid = (right - left) / 2 + left;
+                long time = getTime(piles, mid);
+                if (time <= h) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            return left;
+        }
+
+        public long getTime(int[] piles, int speed) {
+            long time = 0;
+            for (int pile : piles) {
+                time += (pile + speed - 1) / speed;
+            }
+            return time;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new OII0073().new Solution();
+        Object ans = s.minEatingSpeed(new int[]{30, 11, 23, 4, 20}, 5);
+        System.out.println(ans);
+        ans = s.minEatingSpeed(new int[]{30, 11, 23, 4, 20}, 6);
+        System.out.println(ans);
+    }
+}
