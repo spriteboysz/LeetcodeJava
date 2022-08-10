535540fc217d9d13733d6c2dd73bfe590885a365
spriteboysz <sprite_bx@126.com>
2022-08-10 22:21:44 +0800

    2022081022

---
+++ src/leetcode/editor/cn/algorithm/P0289.java
@@ -0,0 +1,50 @@
+package leetcode.editor.cn.algorithm;
+
+import java.util.Arrays;
+
+/**
+ * Author: Deean
+ * Date: 2022-08-10 22:06
+ * FileName: src/leetcode/editor/cn/algorithm
+ * Description: 289. 生命游戏
+ */
+
+public class P0289 {
+    // code beginning
+    class Solution {
+        public void gameOfLife(int[][] board) {
+            int n = board.length, m = board[0].length;
+            int[][] clone = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    int count = 0;
+                    for (int di = -1; di <= 1; di++) {
+                        for (int dj = -1; dj <= 1; dj++) {
+                            if (di == 0 && dj == 0) continue;
+                            if (i + di < 0 || i + di >= n || j + dj < 0 || j + dj >= m) continue;
+                            if (board[i + di][j + dj] == 1) count++;
+                        }
+                    }
+                    if (board[i][j] == 1) {
+                        if (count < 2 || count > 3) {
+                            clone[i][j] = 0;
+                        } else {
+                            clone[i][j] = 1;
+                        }
+                    } else {
+                        if (count == 3) clone[i][j] = 1;
+                    }
+                }
+            }
+            for (int i = 0; i < n; i++) {
+                System.arraycopy(clone[i], 0, board[i], 0, m);
+                System.out.println(Arrays.toString(board[i]));
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new P0289().new Solution();
+        s.gameOfLife(new int[][]{{0, 1, 0}, {0, 0, 1}, {1, 1, 1}, {0, 0, 0}});
+    }
+}
---
+++ src/leetcode/editor/cn/algorithm/P2367.java
@@ -0,0 +1,35 @@
+package leetcode.editor.cn.algorithm;
+
+/**
+ * Author: Deean
+ * Date: 2022-08-10 22:00
+ * FileName: src/leetcode/editor/cn/algorithm
+ * Description:
+ */
+
+public class P2367 {
+    // code beginning
+    class Solution {
+        public int arithmeticTriplets(int[] nums, int diff) {
+            int n = nums.length, count = 0;
+            for (int i = 0; i < n - 2; i++) {
+                for (int j = i + 1; j < n - 1; j++) {
+                    if (nums[j] - nums[i] != diff) continue;
+                    ;
+                    for (int k = j + 1; k < n; k++) {
+                        if (nums[k] - nums[j] == diff) {
+                            count++;
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new P2367().new Solution();
+        Object ans = s.arithmeticTriplets(new int[]{4, 5, 6, 7, 8, 9}, 2);
+        System.out.println(ans);
+    }
+}
