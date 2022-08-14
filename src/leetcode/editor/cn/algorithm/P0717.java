462d769812722d419f4139c2d7b2a157af54ba5d
spriteboysz <sprite_bx@126.com>
2022-08-14 23:08:05 +0800

    2022081423

---
+++ src/leetcode/editor/cn/algorithm/P0717.java
@@ -0,0 +1,32 @@
+package leetcode.editor.cn.algorithm;
+
+/**
+ * Author: Deean
+ * Date: 2022-08-14 22:47
+ * FileName: src/leetcode/editor/cn/algorithm
+ * Description: 717. 1 比特与 2 比特字符
+ */
+
+public class P0717 {
+    // code beginning
+    class Solution {
+        public boolean isOneBitCharacter(int[] bits) {
+            int index = 0;
+            while (index < bits.length - 1) {
+                // if (bits[index] == 1) {
+                //     index += 2;
+                // } else {
+                //     index++;
+                // }
+                index += bits[index] + 1;
+            }
+            return index == bits.length - 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new P0717().new Solution();
+        Object ans = s.isOneBitCharacter(new int[]{1, 1, 1, 0});
+        System.out.println(ans);
+    }
+}
