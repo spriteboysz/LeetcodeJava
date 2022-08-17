2307c938317ee53272038b849cca9744dafc66be
spriteboysz <sprite_bx@126.com>
2022-08-17 23:22:38 +0800

    2022081723

---
+++ src/leetcode/editor/cn/algorithm/P0693.java
@@ -0,0 +1,26 @@
+package leetcode.editor.cn.algorithm;
+
+/**
+ * Author: Deean
+ * Date: 2022-08-17 22:47
+ * FileName: src/leetcode/editor/cn/algorithm
+ * Description: 693. 交替位二进制数
+ */
+
+public class P0693 {
+    // code beginning
+    class Solution {
+        public boolean hasAlternatingBits(int n) {
+            String s = Integer.toBinaryString(n);
+            return !s.contains("11") && !s.contains("00");
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new P0693().new Solution();
+        Object ans = s.hasAlternatingBits(5);
+        System.out.println(ans);
+        ans = s.hasAlternatingBits(7);
+        System.out.println(ans);
+    }
+}
---
+++ src/leetcode/editor/cn/sword/OII0002.java
@@ -0,0 +1,32 @@
+package leetcode.editor.cn.sword;
+
+/**
+ * Author: Deean
+ * Date: 2022-08-17 22:51
+ * FileName: src/leetcode/editor/cn/sword
+ * Description: 剑指 Offer II 002. 二进制加法
+ */
+
+public class OII0002 {
+    // code beginning
+    class Solution {
+        public String addBinary(String a, String b) {
+            int i = a.length() - 1, j = b.length() - 1, carry = 0;
+            StringBuilder builder = new StringBuilder();
+            while (i >= 0 || j >= 0 || carry != 0) {
+                int x = i >= 0 ? a.charAt(i--) - '0' : 0;
+                int y = j >= 0 ? b.charAt(j--) - '0' : 0;
+                int cur = x + y + carry;
+                builder.append(cur % 2);
+                carry = cur / 2;
+            }
+            return builder.reverse().toString();
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new OII0002().new Solution();
+        Object ans = s.addBinary("1010", "1011");
+        System.out.println(ans);
+    }
+}
