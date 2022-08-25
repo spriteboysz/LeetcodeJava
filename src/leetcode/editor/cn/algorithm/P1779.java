0717b88d7272994251bd978164babf82b9cf2fec
spriteboysz <sprite_bx@126.com>
2022-08-25 23:05:42 +0800

    2022082523

--- src/leetcode/editor/cn/algorithm/P1779.java
+++ src/leetcode/editor/cn/algorithm/P1779.java
@@ -13,14 +13,14 @@ public class P1779 {
         public int nearestValidPoint(int x, int y, int[][] points) {
             int minValue = Integer.MAX_VALUE, minIndex = -1;
             for (int i = 0; i < points.length; i++) {
-                if (points[i][0] == x || points[i][1] == y) {
-                    int cur = Math.abs(points[i][0] - x) + Math.abs(points[i][1] - y);
-                    if (cur < minValue) {
-                        minValue = cur;
-                        minIndex = i;
-                    }
+                if (points[i][0] != x && points[i][1] != y) continue;
+                int cur = Math.abs(points[i][0] - x) + Math.abs(points[i][1] - y);
+                if (cur < minValue) {
+                    minValue = cur;
+                    minIndex = i;
                 }
             }
+
             return minIndex;
         }
     }
