b04e8f16173beaca7e41a1da8c854dcb6639deb1
spriteboysz <sprite_bx@126.com>
2023-02-26 13:15:28 +0800

    2023022613

---
+++ src/leetcode/editor/cn/algorithm/P2529.java
@@ -0,0 +1,28 @@
+package leetcode.editor.cn.algorithm;
+
+/**
+ * Author: Deean
+ * Date: 2023-02-26 11:32
+ * FileName: src/leetcode/editor/cn/algorithm
+ * Description: 2529. 正整数和负整数的最大计数
+ */
+
+public class P2529 {
+    // code beginning
+    class Solution {
+        public int maximumCount(int[] nums) {
+            int positive = 0, negative = 0;
+            for (int num : nums) {
+                if (num > 0) positive++;
+                if (num < 0) negative++;
+            }
+            return Math.max(positive, negative);
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new P2529().new Solution();
+        Object ans = s.maximumCount(new int[]{-2, -1, -1, 1, 2, 3});
+        System.out.println(ans);
+    }
+}
