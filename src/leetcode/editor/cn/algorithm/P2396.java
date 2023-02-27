a037b6b6afe2078a363ac1fb28e1fa7ab61aaf62
spriteboysz <sprite_bx@126.com>
2023-02-27 21:30:57 +0800

    2023022721

---
+++ src/leetcode/editor/cn/algorithm/P2396.java
@@ -0,0 +1,23 @@
+package leetcode.editor.cn.algorithm;
+
+/**
+ * Author: Deean
+ * Date: 2023-02-26 22:33
+ * FileName: src/leetcode/editor/cn/algorithm
+ * Description: 2396. 严格回文的数字
+ */
+
+public class P2396 {
+    // code beginning
+    class Solution {
+        public boolean isStrictlyPalindromic(int n) {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new P2396().new Solution();
+        Object ans = s.isStrictlyPalindromic(9);
+        System.out.println(ans);
+    }
+}
