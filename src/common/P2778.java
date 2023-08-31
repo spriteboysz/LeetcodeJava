072fa39d7587b862ea9faba716a9fa3c4d87b075
spriteboysz <sprite_bx@126.com>
2023-08-31 22:50:02 +0800

    2023083122

---
+++ src/common/P2778.java
@@ -0,0 +1,29 @@
+package common;
+
+/**
+ * Author: Deean
+ * Date: 2023-08-30 21:18
+ * FileName: src/common
+ * Description:2778. 特殊元素平方和
+ */
+
+public class P2778 {
+    // code beginning
+    class Solution {
+        public int sumOfSquares(int[] nums) {
+            int sum = 0;
+            for (int i = 0, n = nums.length; i < n; i++) {
+                if (n % (i + 1) == 0) {
+                    sum += nums[i] * nums[i];
+                }
+            }
+            return sum;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new P2778().new Solution();
+        Object ans = s.sumOfSquares(new int[]{2, 7, 1, 19, 18, 3});
+        System.out.println(ans);
+    }
+}
---
+++ src/common/P2785.java
@@ -0,0 +1,41 @@
+package common;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Author: Deean
+ * Date: 2023-08-30 21:04
+ * FileName: src/common
+ * Description:2785. 将字符串中的元音字母排序
+ */
+
+public class P2785 {
+
+    class Solution {
+        public String sortVowels(String s) {
+            List<Character> vowelList = new ArrayList<>();
+            char[] ss = s.toCharArray();
+            String VOWELS = "AEIOUaeiou";
+            for (char c : ss) {
+                if (VOWELS.indexOf(c) >= 0) {
+                    vowelList.add(c);
+                }
+            }
+            Collections.sort(vowelList);
+            for (int i = 0, j = 0; i < ss.length; i++) {
+                if (VOWELS.indexOf(ss[i]) >= 0) {
+                    ss[i] = vowelList.get(j++);
+                }
+            }
+            return String.valueOf(ss);
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new P2785().new Solution();
+        Object ans = s.sortVowels("lEetcOde");
+        System.out.println(ans);
+    }
+}
