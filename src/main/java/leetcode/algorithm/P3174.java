611553cdb10e4781ec7c59cde8a47b91ce2f5f43
spriteboysz <spriteboysz@hotmail.com>
2024-06-16 14:24:15 +0800

    2024061614

---
+++ src/main/java/leetcode/algorithm/P3174.java
@@ -0,0 +1,23 @@
+package leetcode.algorithm;
+
+/**
+ * Author: Deean
+ * Date: 2024-06-16 14:22
+ * FileName: src/main/java/leetcode/algorithm
+ * Description: 3174. 清除数字
+ */
+
+public class P3174 {
+    // code beginning
+    class Solution {
+        public String clearDigits(String s) {
+            return "";
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new P3174().new Solution();
+        var ans = s.clearDigits("cb34");
+        System.out.println(ans);
+    }
+}
