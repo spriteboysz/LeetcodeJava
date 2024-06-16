f41d8e9088ffb0bae3e824155d3ba724b2485630
spriteboysz <spriteboysz@hotmail.com>
2024-06-16 14:27:51 +0800

    2024061614

--- src/main/java/leetcode/algorithm/P3174.java
+++ src/main/java/leetcode/algorithm/P3174.java
@@ -11,7 +11,15 @@ public class P3174 {
     // code beginning
     class Solution {
         public String clearDigits(String s) {
-            return "";
+            StringBuilder ss = new StringBuilder();
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) >= '0' && s.charAt(i) <= '9' && !ss.isEmpty()) {
+                    ss.deleteCharAt(ss.length() - 1);
+                } else {
+                    ss.append(s.charAt(i));
+                }
+            }
+            return ss.toString();
         }
     }
 
